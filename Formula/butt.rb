8900e58e15e41cdf7a8af2a8a2c722c7c1c21956
Chris Farmer <farmerchris@gmail.com>
2026-02-21 15:02:04 -0600

    version

--- Formula/butt.rb
+++ Formula/butt.rb
@@ -1,7 +1,7 @@
 class Butt < Formula
     desc "Throttled tail/stdin monitor with regex highlighting"
     homepage "https://github.com/farmerchris/tailbut"
-    url "https://github.com/farmerchris/tailbut/archive/refs/tags/v0.1.1.tar.gz"
+    url "https://github.com/farmerchris/tailbut/archive/refs/tags/v0.1.2.tar.gz"
     sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
     license "MIT"
 
