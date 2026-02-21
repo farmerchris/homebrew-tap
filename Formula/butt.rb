69f6f34374b6bb0aa581793da8f2e10efc8d325a
Chris Farmer <farmerchris@gmail.com>
2026-02-21 15:23:17 -0600

    M  Formula/butt.rb A  bottle_output.txt A  linkage_output.txt A  skipped_or_failed_formulae-arm64_tahoe.txt

--- Formula/butt.rb
+++ Formula/butt.rb
@@ -1,17 +1,17 @@
 class Butt < Formula
-    desc "Throttled tail/stdin monitor with regex highlighting"
-    homepage "https://github.com/farmerchris/butt"
-    url "https://github.com/farmerchris/butt/archive/refs/tags/v0.1.2.tar.gz"
-    sha256 "9f88cc6decb8861bb8befd9d4561c1c22e624f6c37f3700293ce96944bf33ff6"
-    license "MIT"
+  desc "Throttled tail/stdin monitor with regex highlighting"
+  homepage "https://github.com/farmerchris/butt"
+  url "https://github.com/farmerchris/butt/archive/refs/tags/v0.1.2.tar.gz"
+  sha256 "9f88cc6decb8861bb8befd9d4561c1c22e624f6c37f3700293ce96944bf33ff6"
+  license "MIT"
 
-    depends_on "rust" => :build
+  depends_on "rust" => :build
 
-    def install
-      system "cargo", "install", *std_cargo_args(path: ".")
-    end
-
-    test do
-      assert_match version.to_s, shell_output("#{bin}/butt --version")
-    end
+  def install
+    system "cargo", "install", *std_cargo_args(path: ".")
   end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/butt --version")
+  end
+end
