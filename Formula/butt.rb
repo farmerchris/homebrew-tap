a8b47043906544ff5861f140c700c908567fa95e
Chris Farmer <farmerchris@gmail.com>
2026-02-21 14:59:28 -0600

    Add butt
    A  Formula/butt.rb

---
+++ Formula/butt.rb
@@ -0,0 +1,17 @@
+class Butt < Formula
+    desc "Throttled tail/stdin monitor with regex highlighting"
+    homepage "https://github.com/farmerchris/tailbut"
+    url "https://github.com/farmerchris/tailbut/archive/refs/tags/v0.1.1.tar.gz"
+    sha256 "0019dfc4b32d63c1392aa264aed2253c1e0c2fb09216f8e2cc269bbfb8bb49b5"
+    license "MIT"
+
+    depends_on "rust" => :build
+
+    def install
+      system "cargo", "install", *std_cargo_args(path: "butt")
+    end
+
+    test do
+      assert_match version.to_s, shell_output("#{bin}/butt --version")
+    end
+  end
