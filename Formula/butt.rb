3e35307effcc0bf8ff0cb204bb4d97d9a1823955
Chris Farmer <farmerchris@gmail.com>
2026-02-21 22:10:25 -0600

    butt 0.1.3

--- Formula/butt.rb
+++ Formula/butt.rb
@@ -1,17 +1,56 @@
 class Butt < Formula
   desc "Throttled tail/stdin monitor with regex highlighting"
   homepage "https://github.com/farmerchris/butt"
-  url "https://github.com/farmerchris/butt/archive/refs/tags/v0.1.2.tar.gz"
-  sha256 "9f88cc6decb8861bb8befd9d4561c1c22e624f6c37f3700293ce96944bf33ff6"
-  license "MIT"
-
-  depends_on "rust" => :build
-
-  def install
-    system "cargo", "install", *std_cargo_args(path: ".")
+  version "0.1.3"
+  if OS.mac?
+    if Hardware::CPU.arm?
+      url "https://github.com/farmerchris/butt/releases/download/v0.1.3/butt-aarch64-apple-darwin.tar.xz"
+      sha256 "a1ecf9091106ea9ca460d501004fc669a8e48607d41c4d17a6f51e763c8656fc"
+    end
+    if Hardware::CPU.intel?
+      url "https://github.com/farmerchris/butt/releases/download/v0.1.3/butt-x86_64-apple-darwin.tar.xz"
+      sha256 "a29b26a9502ebb8fd364fad7a25a5b51791c452dbb931872c5f52d5f6ad0f175"
+    end
+  end
+  if OS.linux? && Hardware::CPU.intel?
+      url "https://github.com/farmerchris/butt/releases/download/v0.1.3/butt-x86_64-unknown-linux-gnu.tar.xz"
+      sha256 "91e3826be066bb6cd5617ec791824b6aadde681872fbba3fa6b15a49d89a5485"
   end
 
-  test do
-    assert_match version.to_s, shell_output("#{bin}/butt --version")
+  BINARY_ALIASES = {
+    "aarch64-apple-darwin":     {},
+    "x86_64-apple-darwin":      {},
+    "x86_64-unknown-linux-gnu": {},
+  }.freeze
+
+  def target_triple
+    cpu = Hardware::CPU.arm? ? "aarch64" : "x86_64"
+    os = OS.mac? ? "apple-darwin" : "unknown-linux-gnu"
+
+    "#{cpu}-#{os}"
+  end
+
+  def install_binary_aliases!
+    BINARY_ALIASES[target_triple.to_sym].each do |source, dests|
+      dests.each do |dest|
+        bin.install_symlink bin/source.to_s => dest
+      end
+    end
+  end
+
+  def install
+    bin.install "butt" if OS.mac? && Hardware::CPU.arm?
+    bin.install "butt" if OS.mac? && Hardware::CPU.intel?
+    bin.install "butt" if OS.linux? && Hardware::CPU.intel?
+
+    install_binary_aliases!
+
+    # Homebrew will automatically install these, so we don't need to do that
+    doc_files = Dir["README.*", "readme.*", "LICENSE", "LICENSE.*", "CHANGELOG.*"]
+    leftover_contents = Dir["*"] - doc_files
+
+    # Install any leftover files in pkgshare; these are probably config or
+    # sample files.
+    pkgshare.install(*leftover_contents) unless leftover_contents.empty?
   end
 end
---
+++ README.md
@@ -0,0 +1,45 @@
+# Update Script
+
+```
+  cd ~/src/homebrew-tap
+  git checkout main
+  git pull --ff-only
+
+  gh release download v0.1.3 -D Formula -R farmerchris/butt -p '*.rb' -p '*homebrew*' -p '*formula*' --clobber
+  git diff
+```
+
+Commit
+
+```
+  brew style Formula/butt.rb --fix
+  git add .
+  git commit -m "butt 0.1.3"
+```
+
+Test
+
+```
+  brew untap farmerchris/tap || true
+  brew tap farmerchris/tap "file://$PWD"
+  brew audit --strict --online farmerchris/tap/butt
+  HOMEBREW_NO_AUTO_UPDATE=1 brew reinstall --build-from-source farmerchris/tap/butt
+  brew test farmerchris/tap/butt
+```
+
+Push
+
+```
+
+
+
+  git push origin main
+```
+
+Test
+
+```
+  brew untap farmerchris/tap
+  brew tap farmerchris/tap
+  brew reinstall butt
+111
