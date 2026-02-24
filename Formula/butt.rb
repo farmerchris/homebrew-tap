19850ffd0b8b868ebf2cf2a4e220ab22f6a1f2b2
Chris Farmer <farmerchris@gmail.com>
2026-02-23 22:00:44 -0600

    M  Formula/butt.rb
    Closes #2.
    
    Signed-off-by: Chris Farmer <43839+farmerchris@users.noreply.github.com>

--- Formula/butt.rb
+++ Formula/butt.rb
@@ -4,6 +4,7 @@ class Butt < Formula
   url "https://github.com/farmerchris/butt/archive/refs/tags/v0.1.3.tar.gz"
   sha256 "d84c25843355b88f731872abaaa0c4ee35f164f5cd2c755dfa2723b4615949ff"
   license "MIT"
+  version_scheme 1
 
   depends_on "rust" => :build
 
