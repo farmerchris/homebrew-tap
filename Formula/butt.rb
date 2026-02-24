5999f7785ebc806dd30167dbdff00bc4abe6a584
Chris Farmer <farmerchris@gmail.com>
2026-02-23 21:39:42 -0600

    M  Formula/butt.rb

--- Formula/butt.rb
+++ Formula/butt.rb
@@ -1,19 +1,19 @@
 class Butt < Formula
   desc "Throttled tail/stdin monitor with regex highlighting"
   homepage "https://github.com/farmerchris/butt"
+
   if OS.mac?
     if Hardware::CPU.arm?
       url "https://github.com/farmerchris/butt/releases/download/v0.1.3/butt-aarch64-apple-darwin.tar.xz"
       sha256 "a1ecf9091106ea9ca460d501004fc669a8e48607d41c4d17a6f51e763c8656fc"
-    end
-    if Hardware::CPU.intel?
+    else
       url "https://github.com/farmerchris/butt/releases/download/v0.1.3/butt-x86_64-apple-darwin.tar.xz"
       sha256 "a29b26a9502ebb8fd364fad7a25a5b51791c452dbb931872c5f52d5f6ad0f175"
     end
-  end
-  if OS.linux? && Hardware::CPU.intel?
-      url "https://github.com/farmerchris/butt/releases/download/v0.1.3/butt-x86_64-unknown-linux-gnu.tar.xz"
-      sha256 "91e3826be066bb6cd5617ec791824b6aadde681872fbba3fa6b15a49d89a5485"
+  elsif OS.linux?
+    depends_on arch: :x86_64
+    url "https://github.com/farmerchris/butt/releases/download/v0.1.3/butt-x86_64-unknown-linux-gnu.tar.xz"
+    sha256 "91e3826be066bb6cd5617ec791824b6aadde681872fbba3fa6b15a49d89a5485"
   end
 
   BINARY_ALIASES = {
@@ -38,9 +38,7 @@ class Butt < Formula
   end
 
   def install
-    bin.install "butt" if OS.mac? && Hardware::CPU.arm?
-    bin.install "butt" if OS.mac? && Hardware::CPU.intel?
-    bin.install "butt" if OS.linux? && Hardware::CPU.intel?
+    bin.install "butt"
 
     install_binary_aliases!
 
