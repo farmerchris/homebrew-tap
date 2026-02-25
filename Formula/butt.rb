a14061b20dd88a5a490523a3438f0824a4d3e4e6
Chris Farmer <farmerchris@gmail.com>
2026-02-25 08:34:49 -0600

    M  Formula/butt.rb
    Closes #5.
    
    Signed-off-by: Chris Farmer <43839+farmerchris@users.noreply.github.com>

--- Formula/butt.rb
+++ Formula/butt.rb
@@ -1,15 +1,13 @@
 class Butt < Formula
   desc "Throttled tail/stdin monitor with regex highlighting"
   homepage "https://github.com/farmerchris/butt"
-  url "https://github.com/farmerchris/butt/archive/refs/tags/v0.1.3.tar.gz"
-  sha256 "d84c25843355b88f731872abaaa0c4ee35f164f5cd2c755dfa2723b4615949ff"
+  url "https://github.com/farmerchris/butt/archive/refs/tags/v0.1.4.tar.gz"
+  sha256 "92dfcefeaf4deb132a179b58ceda802213556e427de42b9e5ad6c7d17ce4a9cf"
   license "MIT"
   version_scheme 1
 
   bottle do
-    root_url "https://github.com/farmerchris/homebrew-tap/releases/download/butt-0.1.3"
-    sha256 cellar: :any_skip_relocation, arm64_sequoia: "79e65ccceee4a27e59f055994031733c4bd4049b75d055c2ff058c881f7f13f5"
-    sha256 cellar: :any_skip_relocation, x86_64_linux:  "f4a4e450b707d5dc26636c7006208e817fa288c6b109f9e7a6ae2f93310873bf"
+    root_url "https://github.com/farmerchris/homebrew-tap/releases/download/butt-0.1.4"
   end
 
   depends_on "rust" => :build
